14157e9dd110bd5befa26d47b82678b5f05ca576
Rajan4405 <rajanshatma652@gmail.com>
2023-10-19 16:45:49 +0545

    Creating Home Component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
-import Footer from './components/Footer';
+
 import Header from './components/Header';
 import Home from './components/Home';
-import './styles/app.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Toaster } from "react-hot-toast"
+
+import './styles/app.scss'
+
+
 function App() {
   return <Router>
     <Header />
     <Routes>
       <Route path="/" element={<Home />} />
     </Routes>
-    <Footer />
+    <Toaster />
   </Router>
 }
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,48 @@
 import React from 'react'
-
+import toast from 'react-hot-toast';
+const img1 =
+  "https://9to5mac.com/wp-content/uploads/sites/6/2019/11/apple-16-inch-macbook-pro-deal-1.jpg?quality=82&strip=all";
+const img2 =
+  "https://img.freepik.com/free-photo/one-white-sneaker-shoe-isolated-white_93675-134695.jpg";
 const Home = () => {
+
+  const productList = [
+    {
+      name: "Mac Book",
+      price: 12000,
+      imgSrc: img1,
+      id: "asdfg"
+    },
+    {
+      name: "Shoes",
+      price: 490,
+      imgSrc: img2,
+      id: "asdfgd"
+    },
+  ]
+
+  const addToCartHandler = (options) => {
+    console.log(options);
+    toast.success("Added To cart")
+  }
   return (
-    <div>Home</div>
+    <div className='home'>
+      {
+        productList.map(i => (
+          <ProductCard key={i.id} imgSrc={i.imgSrc} price={i.price} name={i.name} id={i.id} handler={addToCartHandler} />
+        ))
+      }
+    </div>
   )
 }
+
+const ProductCard = ({ name, id, price, handler, imgSrc }) => (
+  <div className="productCard">
+    <img src={imgSrc} alt={name} />
+    <p>{name}</p>
+    <h4>${price}</h4>
+    <button onClick={() => handler({ name, price, id, quantity: 1, imgSrc })}>Add to Cart</button>
+  </div>
+)
 
 export default Home
